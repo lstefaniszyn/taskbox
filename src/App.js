905eda9f4378e07e42473d0c298c8fd99b673a2a
Lukasz Stefaniszyn <lukasz.stefaniszyn@capgemini.com>
2020-07-13 12:34:46 +0200

    storybook example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { WithPinnedTasks } from './components/TaskList.stories';
+import { Provider } from 'react-redux';
+import InboxScreen from './components/InboxScreen';
+import store from './lib/redux';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <InboxScreen/>
+    </Provider>
   );
 }
 
